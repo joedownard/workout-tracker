e8bddd2e0613733b7dd798e4faf5e20f465e73e2
Joe Downard <joehdownard@gmail.com>
2021-04-17 22:23:30 +0100

    Added more days. Added week/day selection page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import {useState} from "react";
-import { Checkbox } from '@material-ui/core';
+import {Checkbox} from '@material-ui/core';
 
 function App() {
-    let week = 0
-    let day = 1
+    const [week, setWeek] = useState(0)
+    const [day, setDay] = useState(4)
 
     const program = [[[{
         name: "Back Squat",
@@ -14,19 +14,19 @@ function App() {
         name: "Back Squat",
         notes: "KEEP BACK ANGLE AND FORM CONSISTENT ACROSS ALL REPS",
         sets: [{reps: 8, RPE: "70%", weight: 60}, {reps: 8, RPE: "70%", weight: 60}]
-    },{
+    }, {
         name: "Overhead Press",
         notes: "RESET EACH REP (DON'T TOUCH-AND-PRESS)",
         sets: [{reps: 8, RPE: "80%", weight: 40}, {reps: 8, RPE: "80%", weight: 40}, {reps: 8, RPE: "80%", weight: 40}]
-    },{
+    }, {
         name: "Lying Leg Curl",
         notes: "FOCUS ON SQUEEZING GLUTES",
         sets: [{reps: 8, RPE: "7", weight: 40}, {reps: 8, RPE: "7", weight: 40}, {reps: 8, RPE: "7", weight: 40}]
-    },{
+    }, {
         name: "Helms Row",
         notes: "STRICT FORM. DRIVE ELBOWS OUT AND BACK AT 45 DEGREE ANGLE",
         sets: [{reps: 12, RPE: "9", weight: 20}, {reps: 12, RPE: "9", weight: 20}, {reps: 12, RPE: "9", weight: 20}]
-    },{
+    }, {
         name: "Hammer Curl",
         notes: "KEEP ELBOWS LOCKED IN PLACE, SQUEEZE THE DUMBBELL HANDLE HARD!",
         sets: [{reps: 20, RPE: "10", weight: 15}, {reps: 20, RPE: "10", weight: 15}, {reps: 20, RPE: "10", weight: 15}]
@@ -34,30 +34,193 @@ function App() {
         [{
             name: "DEADLIFT",
             notes: "CONVENTIONAL OR SUMO: USE WHATEVER STANCE YOU ARE STRONGER WITH",
-            sets: [{reps: 4, RPE: "80%", weight: 100},{reps: 4, RPE: "80%", weight: 100},{reps: 4, RPE: "80%", weight: 100}]
+            sets: [{reps: 4, RPE: "80%", weight: 100}, {reps: 4, RPE: "80%", weight: 100}, {
+                reps: 4,
+                RPE: "80%",
+                weight: 100
+            }]
         }, {
             name: "BARBELL BENCH PRESS",
             notes: "TOP SET. LEAVE 1 (MAYBE 2) REPS IN THE TANK. HARD SET.",
             sets: [{reps: 3, RPE: "85%", weight: 60}]
-        },{
+        }, {
             name: "BARBELL BENCH PRESS",
             notes: "QUICK 1 SECOND PAUSE ON THE CHEST ON EACH REP",
-            sets: [{reps: 10, RPE: "70%", weight: 50}, {reps: 8, RPE: "70%", weight: 50}]
-        },{
+            sets: [{reps: 10, RPE: "70%", weight: 50}, {reps: 10, RPE: "70%", weight: 50}]
+        }, {
             name: "HIP ABDUCTION",
             notes: "MACHINE, BAND OR WEIGHTED, 1 SECOND ISOMETRIC HOLD AT THE TOP OF EACH REP",
             sets: [{reps: 15, RPE: "9", weight: 32}, {reps: 15, RPE: "9", weight: 32}, {reps: 15, RPE: "9", weight: 32}]
-        },{
+        }, {
             name: "WEIGHTED PULL-UP",
             notes: "1.5X SHOULDER WIDTH GRIP, PULL YOUR CHEST TO THE BAR",
-            sets: [{reps: 5, RPE: "8", weight: 20},{reps: 5, RPE: "8", weight: 20},{reps: 5, RPE: "8", weight: 20}]
-        },{
+            sets: [{reps: 5, RPE: "8", weight: 20}, {reps: 5, RPE: "8", weight: 20}, {reps: 5, RPE: "8", weight: 20}]
+        }, {
             name: "STANDING CALF RAISE",
             notes: "1-2 SECOND PAUSE AT THE BOTTOM OF EACH REP, FULL ROM",
-            sets: [{reps: 8, RPE: "9", weight: 100},{reps: 8, RPE: "9", weight: 100},{reps: 8, RPE: "9", weight: 100}]
-        },]]]
+            sets: [{reps: 8, RPE: "9", weight: 100}, {reps: 8, RPE: "9", weight: 100}, {reps: 8, RPE: "9", weight: 100}]
+        },],
+        [{
+            name: "BACK SQUAT",
+            notes: "MAINTAIN TIGHT PRESSURE IN YOUR UPPER BACK AGAINST THE BAR",
+            sets: [{reps: 4, RPE: "80%", weight: 80}, {reps: 4, RPE: "80%", weight: 80}, {
+                reps: 4,
+                RPE: "80%",
+                weight: 80
+            },]
+        }, {
+            name: "WEIGHTED DIP",
+            notes: "DO DUMBBELL FLOOR PRESS IF NO ACCESS TO DIP HANDLES",
+            sets: [{reps: 8, RPE: "8", weight: 20}, {reps: 8, RPE: "8", weight: 20}, {reps: 8, RPE: "8", weight: 20},]
+        }, {
+            name: "HANGING LEG RAISE",
+            notes: "KNEES TO CHEST, CONTROLLED REPS, STRAIGHTEN LEGS MORE TO INCREASE DIFFICULTY",
+            sets: [{reps: 10, RPE: "9", weight: 0}, {reps: 10, RPE: "9", weight: 0}, {reps: 10, RPE: "9", weight: 0},]
+        }, {
+            name: "DB PULL-OVER",
+            notes: "CAN USE A DB, CABLE/ROPE OR BAND, STRETCH AND SQUEEZE LATS!",
+            sets: [{reps: 12, RPE: "8", weight: 24}, {reps: 12, RPE: "8", weight: 24}, {
+                reps: 12,
+                RPE: "8",
+                weight: 24
+            },]
+        }, {
+            name: "INCLINE DUMBBELL CURL",
+            notes: "DO EACH ARM ONE AT A TIME RATHER THAN ALTERNATING, START WITH YOUR WEAK ARM",
+            sets: [{reps: 12, RPE: "9", weight: 15}, {reps: 12, RPE: "9", weight: 15}, {
+                reps: 12,
+                RPE: "9",
+                weight: 15
+            },]
+        }, {
+            name: "FACE PULL",
+            notes: "CAN USE CABLE/ROPE OR BAND, RETRACT YOUR SHOULDER BLADES AS YOU PULL",
+            sets: [{reps: 12, RPE: "9", weight: 40}, {reps: 12, RPE: "9", weight: 40}, {
+                reps: 12,
+                RPE: "9",
+                weight: 40
+            }, {reps: 12, RPE: "9", weight: 40},]
+        },],
+        [{
+            name: "PAUSE DEADLIFT",
+            notes: "3 SECOND PAUSE RIGHT AFTER THE PLATES COME OFF THE GROUND",
+            sets: [{reps: 2, RPE: "75%", weight: 110}, {reps: 2, RPE: "75%", weight: 110}, {
+                reps: 2,
+                RPE: "75%",
+                weight: 110
+            }, {reps: 2, RPE: "75%", weight: 110},]
+        }, {
+            name: "PAUSE BARBELL BENCH PRESS",
+            notes: "2-3 SECOND PAUSE ON THE CHEST",
+            sets: [{reps: 5, RPE: "75%", weight: 60}, {reps: 5, RPE: "75%", weight: 60}, {
+                reps: 5,
+                RPE: "75%",
+                weight: 60
+            },]
+        }, {
+            name: "CHEST-SUPPORTED T-BAR ROW ",
+            notes: "BE MINDFUL OF LOWER BACK FATIGUE. STAY LIGHT, MINIMIZE CHEATING",
+            sets: [{reps: 10, RPE: "7", weight: 50}, {reps: 10, RPE: "7", weight: 50}, {
+                reps: 10,
+                RPE: "7",
+                weight: 50
+            },]
+        }, {
+            name: "LYING LEG CURL",
+            notes: "FOCUS ON SQUEEZING HAMSTRINGS",
+            sets: [{reps: 6, RPE: "8", weight: 45}, {reps: 6, RPE: "8", weight: 45}, {reps: 6, RPE: "8", weight: 45},]
+        }, {
+            name: "DUMBBELL SHRUG",
+            notes: "FEEL A STRETCH ON THE TRAPS AT THE BOTTOM, SQUEEZE HARD AT THE TOP",
+            sets: [{reps: 20, RPE: "9", weight: 15}, {reps: 20, RPE: "9", weight: 15}, {
+                reps: 20,
+                RPE: "9",
+                weight: 15
+            },]
+        },],
+        [{
+            name: "EZ BAR CURL",
+            notes: "CURL THE BAR OUT AND UP IN AN ARC. MINIMIZE MOMENTUM.",
+            sets: [{reps: 12, RPE: "8", weight: 35}, {reps: 12, RPE: "8", weight: 35}, {
+                reps: 12,
+                RPE: "8",
+                weight: 35
+            },]
+        }, {
+            name: "SKULL CRUSHER",
+            notes: "ARC THE BAR BACK BEHIND YOUR HEAD, SOFT TOUCH ON THE FLOOR BEHIND YOU",
+            sets: [{reps: 12, RPE: "8", weight: 35}, {reps: 12, RPE: "8", weight: 35}, {
+                reps: 12,
+                RPE: "8",
+                weight: 35
+            },]
+        }, {
+            name: "CABLE CURL (REVERSE 21s)",
+            notes: "DO BOTH ARMS AT ONCE: 7 REPS FULL ROM, 7 REPS TOP 1/2, 7 REPS BOTTOM 1/2",
+            sets: [{reps: 21, RPE: "10", weight: 40}, {reps: 21, RPE: "7", weight: 40}, {
+                reps: 21,
+                RPE: "7",
+                weight: 40
+            },]
+        }, {
+            name: "TRICEPS PRESSDOWN (REVERSE 21s)",
+            notes: "DO BOTH ARMS AT ONCE: 7 REPS FULL ROM, 7 REPS BOTTOM 1/2, 7 REPS TOP 1/2",
+            sets: [{reps: 21, RPE: "10", weight: 45}, {reps: 21, RPE: "10", weight: 45}, {
+                reps: 21,
+                RPE: "10",
+                weight: 45
+            },]
+        }, {
+            name: "DUMBBELL LATERAL RAISE",
+            notes: "ARC THE DUMBBELL OUT, MIND-MUSCLE CONNECTION WITH MIDDLE FIBERS",
+            sets: [{reps: 20, RPE: "9", weight: 10}, {reps: 20, RPE: "9", weight: 10}, {
+                reps: 20,
+                RPE: "9",
+                weight: 10
+            },]
+        }, {
+            name: "DUMBBELL SHRUG",
+            notes: "1-2 SECOND PAUSE AT THE BOTTOM OF EACH REP, FULL SQUEEZE AT THE TOP",
+            sets: [{reps: 12, RPE: "9", weight: 20}, {reps: 12, RPE: "9", weight: 20}, {
+                reps: 12,
+                RPE: "9",
+                weight: 20
+            },]
+        },],]]
+    const exerciseList = program[week][day].map((exercise, index) => <Exercise dayProgram={program[week][day]}
+                                                                               key={index} pageNumber={index + 1}/>)
     return (
-            program[week][day].map((exercise, index) => <Exercise dayProgram={program[week][day]} pageNumber={index+1}/>)
+        <div>
+            <DayWeekSelectionPage setWeek={setWeek} setDay={setDay} currDay={day} currWeek={week} program={program}/>
+            <div>
+                {exerciseList}
+            </div>
+        </div>
+    );
+}
+
+function DayWeekSelectionPage(props) {
+    const dayButtons = []
+    for (let i = 1; i < props.program[props.currWeek].length+1; i++) {
+        dayButtons.push(<button className={props.currDay === i-1 ? "dayWeekButtonSelected" : "dayWeekButton"} key={i} onClick={() => props.setDay(i-1)}>{i}</button>)
+    }
+
+    const weekButtons = []
+    for (let i = 1; i < props.program.length+1; i++) {
+        weekButtons.push(<button className={props.currWeek === i-1 ? "dayWeekButtonSelected" : "dayWeekButton"} key={i} onClick={() => props.setWeek(i-1)}>{i}</button>)
+    }
+
+    return (
+        <div className="DayWeekSelectionPage">
+            <div className="dayWeekContainer">
+            <h2 className="promptHeader">Select a day and a week</h2>
+            <h3>Week</h3>
+            {weekButtons}
+
+            <h3>Day</h3>
+                {dayButtons}
+            </div>
+        </div>
     );
 }
 
@@ -66,19 +229,19 @@ function Exercise(props) {
     const [time, setTime] = useState(0)
     const [timerInterval, setTimerInterval] = useState()
 
-    function startTimer () {
+    function startTimer() {
         if (timerInterval) {
             clearInterval(timerInterval)
             setTimerInterval()
         }
         const date = new Date()
         const incrementTimer = setInterval(() => {
-            setTime(((new Date().getTime()) - date.getTime())/1000)
+            setTime(((new Date().getTime()) - date.getTime()) / 1000)
         }, 1000)
         setTimerInterval(incrementTimer)
     }
 
-    function stopTimer () {
+    function stopTimer() {
         if (timerInterval) {
             clearInterval(timerInterval)
             setTimerInterval()
@@ -103,7 +266,8 @@ function Exercise(props) {
             <div className="mainContent">
                 <p className="exerciseName">{exerciseName}</p>
                 <div className="sets">
-                    {sets.map((set, index) => <Set id={index} reps={set.reps} RPE={set.RPE} weight={set.weight} startTimer={startTimer} stopTimer={stopTimer}/>)}
+                    {sets.map((set, index) => <Set id={index} key={index} reps={set.reps} RPE={set.RPE} weight={set.weight}
+                                                   startTimer={startTimer} stopTimer={stopTimer}/>)}
                 </div>
                 <div className="notes">
                     <header className="notesHeader">NOTES</header>
